a7b86663412cd0561a2f35b25674ddc7fd1750d0
Lyuda Barabash <barabashlyuda@gmail.com>
2020-06-03 20:46:54 +0300

    Add add functionality

--- src/components/AddItem/index.js
+++ src/components/AddItem/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './AddItem.scss';
 
 const AddItem = () => {
+  const [checked, setChecked] = useState(false);
+
   return (
     <div className="add-item-wrapper">
-      {/* <input className="input-field" />
-      <input type="submit" value="Add" className="add"/> */}
-      <div className="circle">
-        <img alt="Plus" src="plus.png" className="plus-mark"/>
-      </div>
+      {checked ? (
+        <>
+          <input className="input-field" />
+          <div className="buttons-wrapper">
+            <button type="submit" className="button add">Add</button>
+            <button type="submit" className="button cancel" onClick={() => setChecked(false)}>Cancel</button>
+          </div>
+        </>
+      ) : (
+        <div className="circle" onClick={() => setChecked(true)}>
+          <img alt="Plus" src="plus.png" className="plus-mark"/>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/ListWrapper/index.js
+++ src/components/ListWrapper/index.js
@@ -3,13 +3,29 @@ import React from 'react';
 import Item from '../Item';
 import './ListWrapper.scss';
 
+const itemsList = [
+  {
+    text: "Go shopping",
+    checked: false,
+  },
+  {
+    text: "Dancing",
+    checked: false,
+  },
+  {
+    text: "Have lunch",
+    checked: false,
+  },
+  {
+    text: "Clean the room",
+    checked: true,
+  }
+];
+
 const ListWrapper = () => {
   return (
     <div className="list-wrapper">
-      <Item text="Go shopping"/>
-      <Item text="Dancing" />
-      <Item text="Have lunch" />
-      <Item text="Have lunch" checked />
+      {itemsList.map(item => <Item text={item.text} checked={item.checked}/>)}
     </div>
   );
 }
